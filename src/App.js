df9767b1cdf38d56d60aa56185063c19d1511ea5
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-03-13 08:57:06 -0500

    under construction page placed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from '../src/Images/logo.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='welcome'>
+       <img width={100} src={logo} />
+        <h1>Welcome to Travisty Creations</h1>
+        <p>This site hasn't launched yet, but if you are patient enough to wait and keep watching, it may be the home to some of the best stories you've ever experienced</p>
+        <img className='cat' src='https://www.thesprucepets.com/thmb/Dm_fEN3vXtW3lsNo_A0gKaO2LsY=/3956x2628/filters:fill(auto,1)/cat-meowing-on-wooden-floor-125026653-5aeb0df043a103003619c114.jpg' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
