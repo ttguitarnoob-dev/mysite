7aff36857d74d268ebd9479b3daa1c5c1e2224a9
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-03-13 12:32:06 -0500

    updated content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className='welcome'>
        <img width={100} src={logo} />
         <h1>Welcome to Travisty Creations</h1>
-        <p>This site hasn't launched yet, but if you are patient enough to wait and keep watching, it may be the home to some of the best stories you've ever experienced</p>
+        <p>This site hasn't launched yet, but if you are patient enough to wait and keep watching, you may find something cool!</p>
         <img className='cat' src='https://www.thesprucepets.com/thmb/Dm_fEN3vXtW3lsNo_A0gKaO2LsY=/3956x2628/filters:fill(auto,1)/cat-meowing-on-wooden-floor-125026653-5aeb0df043a103003619c114.jpg' />
       </div>
     </div>
